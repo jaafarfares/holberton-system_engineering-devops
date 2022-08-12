6b2c7150566c5fa675348a1eb1e321c7b4f4b3b8
jaafarfares <jaafarferes@gmail.com>
2022-08-12 14:37:36 +0100

    kill process using pkill

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,6 @@
 # kill a process you must use pkill and exec
 
-exec { 'killmenow':
-  command => 'pkill -f killmenow'
+exec { 'kill-proc'
+  command => 'pkill -f killmenow',
+  path => '/usr/bin/'
 }
