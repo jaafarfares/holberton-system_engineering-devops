90639fb7cfb06914e365d6b87aa1a5571ea197dc
jaafarfares <jaafarferes@gmail.com>
2022-08-12 14:55:40 +0100

    kill process using pkill

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
-# kill a process you must use pkill and exec
+# kill a process with puppet and you must use pkill and exec
 
 exec { 'killmenow':
   command => 'pkill -f killmenow',
-  path => '/usr/bin'
+  path        => '/usr/bin'
 }
