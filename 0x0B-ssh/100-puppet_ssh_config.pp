519b9b4b848e953f2330fa25656fc3c95c8ad198
jaafarfares <jaafarferes@gmail.com>
2022-08-14 17:02:57 +0100

    set up your client SSH configuration file so that you can connect to a server without typing a password

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,13 +3,11 @@ file_line { 'ssh_config':
     ensure   => present,
     path     => '/etc/ssh/ssh_config',
     line     => "PasswordAuthentication no",
-    match  => 'PasswordAuthentication yes',
-    multiple => 'true'
+    match  => 'PasswordAuthentication yes'
 }
 ssh_connect { 'ssh_config1':
     ensure   => present,
     path     => '/etc/ssh/ssh_config',
     line     => "IdentityFile ~/.ssh/school",
-    match  => 'IdentityFile ~/.ssh/school',
-    multiple => 'true'
+    match  => 'IdentityFile ~/.ssh/school'
 }
