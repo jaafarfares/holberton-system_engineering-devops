31ef260009836c714d60548136459e73c289d37b
jaafarfares <jaafarferes@gmail.com>
2022-08-15 10:34:58 +0100

    testiiing

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -5,7 +5,7 @@ file_line { 'ssh_config':
     line     => 'PasswordAuthentication no',
     multiple => 'true'
 }
-file_line { 'ssh_config_2':
+file_line { 'ssh_config':
     ensure   => present,
     path     => '/etc/ssh/ssh_config',
     line     => 'IdentityFile ~/.ssh/school',
