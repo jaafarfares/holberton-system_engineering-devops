27bf8ff3cc03afb2c29948cb0f73bddd7832bba4
jaafarfares <jaafarferes@gmail.com>
2022-08-15 10:34:10 +0100

    testiiing

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,13 +1,13 @@
 # connect to a server without typing a password using puppet
 file_line { 'ssh_config':
-    ensure => present,
-    path   => '/etc/ssh/ssh_config',
-    line   => 'PasswordAuthentication no',
-    match  => 'PasswordAuthentication yes'
+    ensure   => present,
+    path     => '/etc/ssh/ssh_config',
+    line     => 'PasswordAuthentication no',
+    multiple => 'true'
 }
-file_line { 'ssh_config':
-    ensure => present,
-    path   => '/etc/ssh/ssh_config',
-    line   => 'IdentityFile ~/.ssh/school',
-    match  => 'IdentityFile ~/.ssh/school'
+file_line { 'ssh_config_2':
+    ensure   => present,
+    path     => '/etc/ssh/ssh_config',
+    line     => 'IdentityFile ~/.ssh/school',
+    multiple => 'true'
 }
