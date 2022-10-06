8cf5e7b2c443aaa90506205793c1ce867aaa058c
ASUS <jaafarferes@gmail.com>
2022-10-06 18:05:52 +0100

    login with the holberton user

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,10 +2,10 @@
 
 exec { 'hard':
   command => 'sed -i "/holberton hard/s/5/20000/" /etc/security/limits.conf',
-  path    => '/home/'
+  path    => '/usr/local/bin/:/bin/'
 }
 
 exec { 'soft':
   command => 'sed -i "/holberton soft/s/4/20000/" /etc/security/limits.conf',
-  path    => '/home/'
+  path    => '/usr/local/bin/:/bin/'
 }
